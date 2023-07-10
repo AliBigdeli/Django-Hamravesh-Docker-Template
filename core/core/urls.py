4e950f146c1e4cbecda8f20147c566edd7e51de2
TheAliBigdeli <bigdeli.ali3@gmail.com>
2023-07-11 01:27:48 +0330

    Update urls.py

--- core/core/urls.py
+++ core/core/urls.py
@@ -60,9 +60,9 @@ if settings.COMINGSOON:
 
 
 # serving medias and static files in debug mode
-
-urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 
 # showing debugger toolbar when you are using debug mode
