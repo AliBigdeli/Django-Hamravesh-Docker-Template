b59dae02dc8a8c806658348120d873f90a14e11c
TheAliBigdeli <bigdeli.ali3@gmail.com>
2023-07-10 23:26:15 +0330

    added model for photo upload for testing disks

--- core/website/admin.py
+++ core/website/admin.py
@@ -1,3 +1,5 @@
-# from django.contrib import admin
-
+from django.contrib import admin
+from .models import Photo
 # Register your models here.
+
+admin.site.register(Photo)
--- core/website/models.py
+++ core/website/models.py
@@ -1,3 +1,6 @@
-# from django.db import models
+from django.db import models
 
 # Create your models here.
+
+class Photo(models.Model):
+    file = models.ImageField(upload_to="photos/")
